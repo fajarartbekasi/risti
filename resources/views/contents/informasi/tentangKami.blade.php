6ca6df608ef59b9c5c2b78e0bd83faa231200eb5
S <fajarartbekasi@gmail.com>
2019-12-04 19:08:43 +0700

    revisi tentang kami

--- resources/views/contents/informasi/tentangKami.blade.php
+++ resources/views/contents/informasi/tentangKami.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
 <section class="jumbotron text-left">
-    <figure class="snip1578">
-        <img src="{{asset('asset/tailor-otwell.jpeg')}}" alt="profile-sample6" />
-        <figcaption>
-            <h2 class="font-weight-bold">Sudiyono Ali</h2>
-            <h5>direktur di PT. Yosiko Indonesia</h5>
-            <h5 class="text-muted">Provinsi Jawa Barat, Indonesia</h5>
-        </figcaption>
-    </figure>
     <h1 class="jumbotron-heading">PT. YOSIKO INDONESIA</h1>
     <p class="lead text-muted">
         PT. Yosiko Indonesia merupakan perusahaan yang  berdiri pada tanggal 3 juli 2009 dan didirikan oleh Bapak Sudiyono, Kantor PT. Yosiko Indonesia
